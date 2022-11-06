8592b8c07abc0a906e51c6fc845b13a71abe69c6
Jiez <jieztee@gmail.com>
2022-11-06 16:18:51 +0800

    insert method

--- Linked List/linked_list.py
+++ Linked List/linked_list.py
@@ -22,10 +22,22 @@ class SingleLinkedList:
         if self.head == None:
             print("No Data")
         else:
+            print('--- Start Print Nodes ---')
             pointer = self.head
             while pointer:
                 print(pointer.val)
                 pointer = pointer.next
+            print('--- Print Nodes End  ---')
+    
+    def insert(self, data, position):
+        node = ListNode(data)
+        pointer = self.head
+        position -= 1;
+        while position != 1:
+            pointer = pointer.next
+            position -= 1
+        node.next = pointer.next
+        pointer.next = node
 
     def append(self, data):
         node = ListNode(data)
@@ -37,6 +49,7 @@ class SingleLinkedList:
                 pointer = pointer.next
             
             pointer.next = node
+    
     def push(self, data):
         node = ListNode(data)
         if self.head:
@@ -50,5 +63,10 @@ if __name__ == '__main__':
     linked_list.append(98)
     linked_list.append(3)
     linked_list.append(67)
+    linked_list.print_data()
+    
     linked_list.push(53) 
+    linked_list.print_data()
+
+    linked_list.insert(76, 3)
     linked_list.print_data()
